941324406aa747ef517fe5e25cc5b791505d4142
supriti.dan <supriti.dan@rakuten.com>
2016-09-19 10:26:21 +0900

    first commit for my rabbitMQ learning

---
+++ src/Recv.java
@@ -0,0 +1,32 @@
+/**
+ * Created by supriti.dan on 17/09/2016.
+ */
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+
+
+//Example of Hello world using rabbitMQ
+public class Recv {
+      private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+                    throws IOException {
+                String message = new String(body, "UTF-8");
+                System.out.println(" [x] Received '" + message + "'");
+            }
+        };
+        channel.basicConsume(QUEUE_NAME, true, consumer);
+    }
+}
